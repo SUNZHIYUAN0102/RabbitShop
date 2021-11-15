621c26ce5ef604fe2d7eb6b5cd578c74ee2ffc94
SUNZHIYUAN0102 <zhiyuansun703@gmail.com>
2021-11-15 23:19:24 +0700

    initialize project

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        user: 'meow'
+    },
+    getters: {
+
+    },
+    mutations: {},
+    actions: {},
+    modules: {}
 })
