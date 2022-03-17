3ecf182b6ac92e54a7eb0ee3687a9ed8b762ed8d
SUNZHIYUAN0102 <zhiyuansun703@gmail.com>
2022-03-17 20:59:06 +0700

    Finish app-header-cart build

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -5,6 +5,19 @@ export default {
             list: []
         }
     },
+    getters: {
+        validList(state) {
+            return state.list.filter(goods => goods.stock > 0 && goods.isEffective)
+        },
+
+        validTotal(state, getters) {
+            return getters.validList.reduce((p, c) => p + c.count, 0)
+        },
+
+        validAmount(state, getters) {
+            return getters.validList.reduce((p, c) => p + c.count * c.nowPrice * 100, 0) / 100
+        }
+    },
     mutations: {
         insertCart(state, payload) {
             const sameIndex = state.list.findIndex(goods => goods.skuId === payload.skuId)
