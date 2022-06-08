c8f76bdbdb822b247b45de5043e29ad4c71f66cf
SUNZHIYUAN0102 <zhiyuansun703@gmail.com>
2022-06-08 20:22:08 +0700

    finish render all orders data

--- src/api/order.js
+++ src/api/order.js
@@ -1,21 +1,25 @@
 import request from '@/utils/request'
 
 export const createOrder = () => {
-    return request('/member/order/pre', 'get')
+  return request('/member/order/pre', 'get')
 }
 
 export const addAddress = (form) => {
-    return request('/member/address', 'post', form)
+  return request('/member/address', 'post', form)
 }
 
 export const editAddress = (form) => {
-    return request(`/member/address/${form.id}`, 'put', form)
+  return request(`/member/address/${form.id}`, 'put', form)
 }
 
 export const submitOrder = (params) => {
-    return request('/member/order', 'post', params)
+  return request('/member/order', 'post', params)
 }
 
 export const findOrderDetail = (orderId) => {
-    return request(`/member/order/${orderId}`, 'get')
+  return request(`/member/order/${orderId}`, 'get')
+}
+
+export const findOrderList = ({ page = 1, pageSize = 10, orderState = 0 }) => {
+  return request('/member/order', 'get', { page, pageSize, orderState })
 }
